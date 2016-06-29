5b117e5322b0eb8f8f29891c54495a4ba6b129e1
Sam Wu <Wu@Sams-MacBook-Air.local>
2016-06-29 12:31:15 -0400

    Initial commit

---
+++ Remainder/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Remainder
+//
+//  Created by Sam Wu on 6/29/16.
+//  Copyright © 2016 BothPoints. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
